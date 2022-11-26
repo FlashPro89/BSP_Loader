050e3d40a1a1484d531564d7d524b831e66e48af
flash <flash@unigine.com>
2022-11-26 12:44:56 +0400

    fixed gTimer bug

--- Timer.cpp
+++ Timer.cpp
@@ -16,7 +16,7 @@ float gTimer::getDelta()
 	LARGE_INTEGER current;
 	QueryPerformanceCounter( &current );
 
-	float delta = ( (float)current.QuadPart - (float)m_last.QuadPart ) / (float)m_freq.QuadPart;
+	float delta = ( current.QuadPart - m_last.QuadPart ) / (float)m_freq.QuadPart;
 	m_last = current;
 	return delta;
 }
